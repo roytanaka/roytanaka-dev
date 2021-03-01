417fa8498e062c5c274a08a47614ea89b06b20d9
Roy Tanaka <tanaka@mac.com>
2021-03-01 18:50:53 -0500

    site config

--- src/main.js
+++ src/main.js
@@ -2,9 +2,20 @@ import Vuetify from "vuetify";
 import DefaultLayout from "~/layouts/Default.vue";
 
 import "vuetify/dist/vuetify.min.css";
-import colors from "vuetify/lib/util/colors";
+// import colors from "vuetify/lib/util/colors";
 
 export default function(Vue, { appOptions, head }) {
+  
+  head.link.push({
+    rel: "preconnect",
+    href: "https://fonts.gstatic.com"
+  });
+
+  head.link.push({
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
+  });
+
   head.link.push({
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/icon?family=Material+Icons"
@@ -22,8 +33,12 @@ export default function(Vue, { appOptions, head }) {
     theme: {
       themes: {
         light: {
-          primary: colors.teal.darken2,
-          secondary: "#424242",
+          primary: '#7298c1',
+          primaryLight: '#a3c9f4',
+          primaryDark: '#426a91',
+          secondary: "#9e9e9e",
+          secondaryLight: '#cfcfcf',
+          secondaryDark: '#707070',
           accent: "#82B1FF",
           error: "#FF5252",
           info: "#2196F3",
