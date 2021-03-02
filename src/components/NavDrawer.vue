a21914de257710a69d9da22c8764767d8a049574
Roy Tanaka <tanaka@mac.com>
2021-03-01 22:36:08 -0500

    profile

--- src/components/NavDrawer.vue
+++ src/components/NavDrawer.vue
@@ -1,9 +1,51 @@
 <template>
-  <v-navigation-drawer color="primaryDark" width="400" :value="value" app>
-    <v-card flat tile class="transparent">
-      <v-card-title primary-title>
-        hello
+  <v-navigation-drawer color="secondaryDark" width="400" :value="value" app>
+    <v-card class="transparent mt-12" flat tile>
+      <v-row justify="center">
+        <v-avatar size="200">
+          <img
+            src="/images/roy-tanaka.jpg"
+            alt="Roy TanFrontend Developeraka"
+          />
+        </v-avatar>
+      </v-row>
+      <v-card-title class="justify-center">
+        <h1 class="profile profile-name">ROY TANAKA</h1>
+        <h2 class="profile profile-title">Frontend Web Developer</h2>
       </v-card-title>
+      <v-card-text class="text-center">
+        <v-btn
+          v-for="social in socials"
+          :key="social.icon"
+          class="mx-2 white--text"
+          icon
+          :href="social.link"
+          target="_blank"
+        >
+          <v-icon size="24px">
+            {{ social.icon }}
+          </v-icon>
+        </v-btn>
+      </v-card-text>
+    </v-card>
+    <v-card flat tile class="transparent px-12">
+      <v-card-title class="profile">
+        Skills
+      </v-card-title>
+      <v-card-text>
+        <v-progress-linear
+          class="mb-1"
+          color="primaryLight"
+          height="28"
+          v-for="skill in skills"
+          :key="skill.name"
+          :value="skill.score"
+        >
+          <template
+            ><div class="ml-3 mr-auto">{{ skill.name }}</div></template
+          >
+        </v-progress-linear>
+      </v-card-text>
     </v-card>
   </v-navigation-drawer>
 </template>
@@ -13,7 +55,68 @@ export default {
   props: {
     value: Boolean,
   },
+  data: () => ({
+    socials: [
+      {
+        icon: 'fab fa-linkedin',
+        link: 'https://www.linkedin.com/in/roytanaka/',
+      },
+      { icon: 'fab fa-github', link: 'https://github.com/roytanaka/' },
+      { icon: 'fab fa-twitter', link: 'https://twitter.com/roytanaka/' },
+    ],
+    skills: [
+      { name: 'JavaScript (ES6+)', score: 0 },
+      { name: 'Vue.js', score: 0 },
+      { name: 'Node.js', score: 0 },
+      { name: 'React', score: 0 },
+      { name: 'HTML', score: 0 },
+      { name: 'CSS', score: 0 },
+      { name: 'Git', score: 0 },
+      { name: 'SQL (MySQL)', score: 0 },
+      { name: 'RESTful API', score: 0 },
+      { name: 'Photoshop', score: 0 },
+      { name: 'Illustrator', score: 0 },
+      { name: 'Attention to detail', score: 0 },
+      { name: 'Public speaking', score: 0 },
+      { name: 'Critical thinking', score: 0 },
+    ],
+  }),
+  created() {
+    setTimeout(() => {
+      this.skills = [
+        { name: 'JavaScript (ES6+)', score: 90 },
+        { name: 'Vue.js', score: 80 },
+        { name: 'Node.js', score: 75 },
+        { name: 'React', score: 60 },
+        { name: 'HTML', score: 92 },
+        { name: 'CSS', score: 80 },
+        { name: 'Git', score: 75 },
+        { name: 'SQL (MySQL)', score: 55 },
+        { name: 'RESTful API', score: 75 },
+        { name: 'Photoshop', score: 80 },
+        { name: 'Illustrator', score: 80 },
+        { name: 'Attention to detail', score: 90 },
+        { name: 'Public speaking', score: 85 },
+        { name: 'Critical thinking', score: 90 },
+      ];
+    }, 500);
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+.profile {
+  color: white;
+}
+.profile-name {
+  font-weight: 100;
+  font-size: 2.5rem;
+}
+.profile-title {
+  font-family: 'Roboto Condensed';
+  font-weight: 300;
+}
+.v-progress-linear {
+  transition: 1s ease-in-out;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,33 +1,37 @@
-import Vuetify from "vuetify";
-import DefaultLayout from "~/layouts/Default.vue";
+import Vuetify from 'vuetify';
+import DefaultLayout from '~/layouts/Default.vue';
 
-import "vuetify/dist/vuetify.min.css";
+import 'vuetify/dist/vuetify.min.css';
 // import colors from "vuetify/lib/util/colors";
 
 export default function(Vue, { appOptions, head }) {
-  
   head.link.push({
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com"
+    rel: 'preconnect',
+    href: 'https://fonts.gstatic.com',
   });
 
   head.link.push({
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
+    rel: 'stylesheet',
+    href:
+      'https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap',
   });
 
   head.link.push({
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/icon?family=Material+Icons"
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/icon?family=Material+Icons',
   });
   head.link.push({
-    rel: "stylesheet",
-    href: "https://use.fontawesome.com/releases/v5.10.2/css/all.css"
+    rel: 'stylesheet',
+    href: 'https://use.fontawesome.com/releases/v5.10.2/css/all.css',
+  });
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://use.fontawesome.com/releases/v5.10.2/css/brands.css',
   });
 
   const opts = {
     icons: {
-      iconfont: "fa" // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4'
+      iconfont: 'fa', // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4'
     },
     // adjust the theme if you would like to
     theme: {
@@ -36,22 +40,22 @@ export default function(Vue, { appOptions, head }) {
           primary: '#7298c1',
           primaryLight: '#a3c9f4',
           primaryDark: '#426a91',
-          secondary: "#9e9e9e",
+          secondary: '#9e9e9e',
           secondaryLight: '#cfcfcf',
           secondaryDark: '#707070',
-          accent: "#82B1FF",
-          error: "#FF5252",
-          info: "#2196F3",
-          success: "#4CAF50",
-          warning: "#FFC107"
-        }
-      }
-    }
+          accent: '#82B1FF',
+          error: '#FF5252',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FFC107',
+        },
+      },
+    },
   };
   Vue.use(Vuetify);
 
   appOptions.vuetify = new Vuetify(opts);
 
   // Set default layout as a global component
-  Vue.component("Layout", DefaultLayout);
+  Vue.component('Layout', DefaultLayout);
 }
