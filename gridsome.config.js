06e750dd7209c71f68e5a34f59262dffc8bb83d1
Roy Tanaka <tanaka@mac.com>
2021-03-07 18:44:46 -0500

    update webpack-node-externals

--- gridsome.config.js
+++ gridsome.config.js
@@ -8,35 +8,6 @@ module.exports = {
   siteName: 'Roy Tanaka dot Dev',
   siteUrl: 'https://roytanaka.dev',
   icon: './src/r-favicon.png',
-  plugins: [
-    {
-      use: '@gridsome/source-filesystem',
-      options: {
-        path: 'articles/**/*.md',
-        typeName: 'Article',
-        resolveAbsolutePaths: true,
-        remark: {
-          externalLinksTarget: '_blank',
-          externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-        },
-      },
-    },
-    {
-      use: '@gridsome/source-filesystem',
-      options: {
-        path: 'resume/**/*.md',
-        typeName: 'Resume',
-        resolveAbsolutePaths: true,
-        remark: {
-          externalLinksTarget: '_blank',
-          externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-        },
-      },
-    },
-  ],
-  transformers: {
-    remark: {
-      plugins: ['@gridsome/remark-prismjs'],
-    },
-  },
+
+  plugins: [],
 };
